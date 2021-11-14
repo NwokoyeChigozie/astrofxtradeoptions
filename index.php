cd0ed3980cca20dd215ffdfe9e50bdb30e0545be
Nwokoye Chigozie <gregoflash05@gmail.com>
2021-11-14 12:02:54 +0100

    updates

--- index.php
+++ index.php
@@ -2435,17 +2435,7 @@ s0.parentNode.insertBefore(s1,s0);
     //    print_r($regt_diff);
     ?>
     <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6190eafb6885f60a50bbb8cd/1fkf1bre6';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
+    <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="b6184581-1571-4feb-aa2b-81ef8b799599";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
 <!--End of Tawk.to Script-->
 </body>
 
